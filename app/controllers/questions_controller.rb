e42140a1fab362a0c361982e3164bd29bb6340e3
Tammam Kbeili <tammam.kbeili@gmail.com>
2017-02-13 21:34:24 -0800

    refactor the questions controller / views

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,19 +1,27 @@
 class QuestionsController < ApplicationController
+  # this will execute the method `find_question` before it executes the action
+  # if you give it `only` or `except` options then it will only include or only
+  # exclude certain actions
+  # remember that `before_action` methods get executed within the same
+  # request/response cycle which means if you define an instance variable in
+  # the `before_action` method, the variable will be available in the action
+  before_action(:find_question, { only: [:show, :edit, :destroy, :update] })
+
   def new
     @question = Question.new
     # by default this will render app/views/questions/new.html.erb
   end
 
   def create
-    # this feature is called strong parameters (introduced in Rails 4+)
-    question_params = params.require(:question).permit([:title, :body])
     @question  = Question.new(question_params)
     if @question.save
       # render :show
       # redirect_to question_path({ id: @question.id })
       # redirect_to question_path({ id: @question })
+      flash[:notice] = 'Question created successfully'
       redirect_to question_path(@question)
     else
+      flash.now[:alert] = 'Please fix errors below'
       # this will force Rails to render: app/views/questions/new.html.erb
       # instead of the default: app/views/questions/create.html.erb
       render :new
@@ -21,7 +29,6 @@ class QuestionsController < ApplicationController
   end
 
   def show
-    @question = Question.find params[:id]
   end
 
   def index
@@ -29,22 +36,30 @@ class QuestionsController < ApplicationController
   end
 
   def edit
-    @question = Question.find params[:id]
   end
 
   def update
-    @question = Question.find params[:id]
-    question_params = params.require(:question).permit([:title, :body])
     if @question.update question_params
-      redirect_to question_path(@question)
+      redirect_to question_path(@question), notice: 'Question updated!'
     else
       render :edit
     end
   end
 
   def destroy
-    question = Question.find params[:id]
-    question.destroy
-    redirect_to questions_path
+    @question.destroy
+    redirect_to questions_path, notice: 'Question deleted!'
   end
+
+  private
+
+  def question_params
+    # this feature is called strong parameters (introduced in Rails 4+)
+    params.require(:question).permit([:title, :body])
+  end
+
+  def find_question
+    @question = Question.find params[:id]
+  end
+
 end
