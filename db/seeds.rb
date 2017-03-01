9822506ccd5e86b9c736fe629742c1d2e9b51f54
Steve Godin <steve.god@gmail.com>
2017-02-28 16:05:41 -0800

    Add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+PASSWORD = 'password123'
+
+10.times do
+  User.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    password: PASSWORD
+  )
+end
+
+users = User.all
+
+100.times do
+  Question.create(
+    title: Faker::Book.title,
+    body: Faker::Hipster.paragraph,
+    user: users.sample
+  )
+end
